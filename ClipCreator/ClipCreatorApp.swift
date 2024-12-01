654952fc6eeeb2d156ce56819f2c7217372dcb44
ARbmgc <markhaddad1017@gmail.com>
2024-12-01 18:26:46 -0500

    Initial commit

---
+++ ClipCreator/ClipCreatorApp.swift
@@ -0,0 +1,33 @@
+//
+//  ClipCreatorApp.swift
+//  ClipCreator
+//
+//  Created by Mark Haddad on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipCreatorApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
